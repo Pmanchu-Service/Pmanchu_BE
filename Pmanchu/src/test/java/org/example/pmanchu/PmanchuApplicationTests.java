f3b7774a8b729078a486315e59eaf8c226207dc9
ByunDohwi <dohwe0978@gmail.com>
2024-10-10 22:31:56 +0900

    (#1) chore:프로젝트 생성

---
+++ Pmanchu/src/test/java/org/example/pmanchu/PmanchuApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.pmanchu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmanchuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
