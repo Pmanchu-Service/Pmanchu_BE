3b4580fc82fe9fcfaa8f09bd39df8ec049297233
Hannah <sunday0206@dsm.hs.kr>
2024-10-11 20:15:12 +0900

    (#2) feat: Project 엔티티 추가

---
+++ src/main/java/org/example/pmanchu/domain/project/domain/Project.java
@@ -0,0 +1,24 @@
+package org.example.pmanchu.domain.project.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "project")
+@Getter
+@NoArgsConstructor
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private String status;
+}
