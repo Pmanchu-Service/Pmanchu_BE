0ad90fff71777c8db7a1ccfc145f94b083784d0b
Hannah <sunday0206@dsm.hs.kr>
2024-10-11 20:26:04 +0900

    (#2) feat: Notification 엔티티 추가

---
+++ src/main/java/org/example/pmanchu/domain/notification/domain/Notification.java
@@ -0,0 +1,24 @@
+package org.example.pmanchu.domain.notification.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "notification")
+@Getter
+@NoArgsConstructor
+public class Notification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String type;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String message;
+}
