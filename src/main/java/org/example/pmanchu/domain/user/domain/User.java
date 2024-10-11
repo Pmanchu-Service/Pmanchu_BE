658da2a11427aafe463c13c386c363832f6f27c1
Hannah <sunday0206@dsm.hs.kr>
2024-10-11 20:14:35 +0900

    (#2) feat: User 엔티티 추가

---
+++ src/main/java/org/example/pmanchu/domain/user/domain/User.java
@@ -0,0 +1,29 @@
+package org.example.pmanchu.domain.user.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user")
+@Getter
+@NoArgsConstructor
+public class User {
+    @Id
+    private Long userId;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(unique = true, nullable = true, name = "github_id")
+    private Long githubId;
+
+    @Column(unique = true, nullable = true, name = "github_username")
+    private String githubUsername;
+
+    @Column(nullable = false)
+    private String introductoin;
+}
