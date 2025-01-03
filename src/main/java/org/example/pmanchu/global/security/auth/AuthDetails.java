9063b154f96866eca392a9147a00c06db6ac1ec6
dohwi <dohwe0978@gmail.com>
2025-01-03 19:28:00 +0900

    add: auth details

---
+++ src/main/java/org/example/pmanchu/global/security/auth/AuthDetails.java
@@ -0,0 +1,50 @@
+package org.example.pmanchu.global.security.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.example.pmanchu.domain.user.domain.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class AuthDetails implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUserId().toString();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
