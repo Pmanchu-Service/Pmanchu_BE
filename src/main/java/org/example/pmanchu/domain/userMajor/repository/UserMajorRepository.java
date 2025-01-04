c0db009cf4950fe9bf1ff8257fe1677bfe937e87
dohwi <dohwe0978@gmail.com>
2025-01-04 23:13:21 +0900

    feat : repository 생성 및 수정

--- src/main/java/org/example/pmanchu/domain/userMajor/repository/UserMajorRepository.java
+++ src/main/java/org/example/pmanchu/domain/userMajor/repository/UserMajorRepository.java
@@ -1,7 +1,8 @@
 package org.example.pmanchu.domain.userMajor.repository;
 
 import org.example.pmanchu.domain.notification.domain.Notification;
+import org.example.pmanchu.domain.userMajor.domain.UserMajor;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserMajorRepository extends CrudRepository<Notification, Long> {
+public interface UserMajorRepository extends CrudRepository<UserMajor, Long> {
 }
---
+++ src/main/java/org/example/pmanchu/domain/userstack/repository/UserStackRepository.java
@@ -0,0 +1,8 @@
+package org.example.pmanchu.domain.userstack.repository;
+
+import org.example.pmanchu.domain.userstack.domain.UserStack;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserStackRepository extends CrudRepository<UserStack, Long> {
+
+}
