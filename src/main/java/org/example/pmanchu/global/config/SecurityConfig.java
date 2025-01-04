6169df6af3d1bc7efecdd8ca83e076d02f5c844f
dohwi <dohwe0978@gmail.com>
2025-01-04 23:11:23 +0900

    fix : url 추가

--- src/main/java/org/example/pmanchu/global/config/SecurityConfig.java
+++ src/main/java/org/example/pmanchu/global/config/SecurityConfig.java
@@ -33,12 +33,13 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth ->
                         auth
                                 .requestMatchers("/auth/login", "/auth/reissue").permitAll()
-                                .requestMatchers("/auth/update").authenticated()
+                                .requestMatchers("/auth/update","auth/delete").authenticated()
                                 .requestMatchers("/swagger-ui/index.html",
                                         // Swagger 허용 URL
                                         "/v2/api-docs", "/v3/api-docs", "/v3/api-docs/**", "/swagger-resources",
                                         "/swagger-resources/**", "/configuration/ui", "/configuration/security", "/swagger-ui/**",
                                         "/webjars/**", "/swagger-ui.html").permitAll()
+                                .anyRequest().authenticated()
                 )
                 .addFilterBefore(new JwtFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(new ExceptionFilter(objectMapper), JwtFilter.class);
