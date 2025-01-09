db30a75e660694bc1bf8724792fbd9727bfedefa
dohwi <dohwe0978@gmail.com>
2025-01-09 16:09:43 +0900

    add: repository deletebyuserid 추가

--- src/main/java/org/example/pmanchu/domain/userLink/repository/UserLinkRepository.java
+++ src/main/java/org/example/pmanchu/domain/userLink/repository/UserLinkRepository.java
@@ -1,7 +1,9 @@
 package org.example.pmanchu.domain.userLink.repository;
 
+import org.example.pmanchu.domain.user.domain.User;
 import org.example.pmanchu.domain.userLink.domain.UserLink;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserLinkRepository extends CrudRepository<UserLink, Long> {
+    void deleteByUserId(User userId);
 }
