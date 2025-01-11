06291c4f0637751a047cc972351c500c261b2618
dohwi <dohwe0978@gmail.com>
2025-01-11 22:01:34 +0900

    feat: jpql 추가

--- src/main/java/org/example/pmanchu/domain/projectMember/repository/ProjectMemberRepository.java
+++ src/main/java/org/example/pmanchu/domain/projectMember/repository/ProjectMemberRepository.java
@@ -1,7 +1,11 @@
 package org.example.pmanchu.domain.projectMember.repository;
 
 import org.example.pmanchu.domain.projectMember.domain.ProjectMember;
+import org.example.pmanchu.domain.user.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ProjectMemberRepository extends CrudRepository<ProjectMember, Long> {
+    List<ProjectMember> findProjectMembersByUserId(User userId);
 }
--- src/main/java/org/example/pmanchu/domain/userstack/repository/UserStackRepository.java
+++ src/main/java/org/example/pmanchu/domain/userstack/repository/UserStackRepository.java
@@ -4,6 +4,9 @@ import org.example.pmanchu.domain.user.domain.User;
 import org.example.pmanchu.domain.userstack.domain.UserStack;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface UserStackRepository extends CrudRepository<UserStack, Long> {
+    List<UserStack> findAllByUserId(User userId);
     void deleteByUserId(User userId);
 }
